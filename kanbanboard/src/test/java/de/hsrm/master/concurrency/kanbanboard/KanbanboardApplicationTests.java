cf345fc217fed220b5e3887e84ccb9bc97372389
Anna-Livia Martin <119300861+AnnaLiviaMartin@users.noreply.github.com>
2026-06-24 09:36:55 +0200

    feat: init backend as spring boot project

---
+++ kanbanboard/src/test/java/de/hsrm/master/concurrency/kanbanboard/KanbanboardApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsrm.master.concurrency.kanbanboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
