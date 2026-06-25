3bd7748e42e4a43267dab7a91ce2eebaaa1c33b7
Anna-Livia Martin <119300861+AnnaLiviaMartin@users.noreply.github.com>
2026-06-25 14:02:53 +0200

    feat: add three Entities

---
+++ kanbanboard/src/main/java/de/hsrm/master/concurrency/kanbanboard/entity/Board.java
@@ -0,0 +1,59 @@
+package de.hsrm.master.concurrency.kanbanboard.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Board {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Version
+    private long version;
+
+    @NotBlank(message = "Board-Name darf nicht leer sein")
+    @Size(min = 1, max = 100, message = "Board-Name muss zwischen {min} und {max} Zeichen lang sein")
+    @NotNull
+    private String name;
+
+    @Column(updatable = false)
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("position ASC")
+    private List<BoardColumn> columns = new ArrayList<>();
+
+    public Board(String name) {
+        this.name = name;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public void addColumn(BoardColumn column) {
+        columns.add(column);
+        column.setBoard(this);
+    }
+
+    public void removeColumn(BoardColumn column) {
+        columns.remove(column);
+        column.setBoard(null);
+    }
+}
